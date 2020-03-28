6417d060fc6545b0f0ac7099c3e8dfed0b2fe9c0
DivLoic <ldivad@isep.fr>
2020-03-28 14:27:11 +0100

    add test execution and CI description

---
+++ src/test/scala/fr/xebia/ldi/crocodile/StreamingAppTest.scala
@@ -0,0 +1,15 @@
+package fr.xebia.ldi.crocodile
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+/**
+ * Created by loicmdivad.
+ */
+class StreamingAppTest extends AnyFlatSpec with Matchers {
+
+  "Application" should "be tested" in {
+    fail()
+  }
+
+}
